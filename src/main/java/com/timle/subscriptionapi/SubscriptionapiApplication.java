a22a52803d923a29e515e00a2aee4bc9b53495cd
timphamle <heytimle@gmail.com>
2026-05-28 00:45:35 -0400

    Initial Spring Boot setup

---
+++ src/main/java/com/timle/subscriptionapi/SubscriptionapiApplication.java
@@ -0,0 +1,13 @@
+package com.timle.subscriptionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timle/subscriptionapi/SubscriptionapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timle.subscriptionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
